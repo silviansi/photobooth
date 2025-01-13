e6e6fbf7bfdc9b065a2e1e4718c30f18e6772603
silviansi <silviansi24@gmail.com>
2025-01-13 20:52:51 +0700

    install vue js

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center">Welcome to My Portfolio</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+</style>
