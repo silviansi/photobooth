f3a129d314ceaf41ce69e2d44c405c34ee9637dd
silviansi <silviansi24@gmail.com>
2025-01-15 21:08:24 +0700

    membuat halaman about

---
+++ resources/js/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1 class="text-2xl font-bold">Tentang Aplikasi Ini</h1>
+      <p>Ini adalah halaman About untuk portofolio Vue dan Laravel Anda.</p>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "AboutView",
+};
+</script>  
