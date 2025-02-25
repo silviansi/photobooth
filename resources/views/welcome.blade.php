972e7b7444302f5203175648034aaf57a3957fa5
silviansi <silviansi24@gmail.com>
2025-02-25 20:42:48 +0700

    impor tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Web Photoshoot</title>
-  @vite('resources/js/app.js')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
   <div id="app"></div>
