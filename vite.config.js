dce835666d8c789424fd443cbddaad1d27203e63
silviansi <silviansi24@gmail.com>
2025-02-26 20:16:14 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-    ],
-});
+  plugins: [vue()],
+  server: {
+    port: 5173,
+  }
+})
