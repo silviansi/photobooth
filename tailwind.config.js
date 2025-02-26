dbb59b10f99514002519cb6856aa084a0dc78758
silviansi <silviansi24@gmail.com>
2025-02-26 20:16:24 +0700

    update setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
